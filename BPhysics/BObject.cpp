0c300c62094e65bbdc6af491865345608f1cd722
Dynesshely <catrol@qq.com>
2022-04-10 00:50:49 +0800

    💾 Feat: BObject

--- BPhysics/BObject.cpp
+++ BPhysics/BObject.cpp
@@ -1,2 +1,11 @@
 #include "BObject.h"
 
+std::string BPhysics::BObject::GetHashCode()
+{
+    return std::string();
+}
+
+std::string BPhysics::BObject::ToString()
+{
+    return std::string();
+}
--- BPhysics/BObject.h
+++ BPhysics/BObject.h
@@ -1,1 +1,21 @@
 #include "../Base/Base.h"
+
+namespace BPhysics{
+
+    /*
+    * Name                  Object's Name for BE Objects Map;
+    * Size                  Size of used memory. 其实 bitset<64> 前32表示整数部分, 后32表示小数部分;
+    *
+    * GetHashCode()         Get a hash by CrasdalHash in `clv = CompressLevel.x32`;
+    * ToString()            If this object can be displayed in String format, than return string
+    *                       else return the object's real type with namespace;
+    */
+
+    class BObject{
+        public:
+        std::string Name;
+        std::bitset<64> Size;
+        std::string GetHashCode();
+        std::string ToString();
+    };
+}
