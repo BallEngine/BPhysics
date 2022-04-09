2a745e584548ebb7d1ee6e435c6ecd13db01bb23
Dynesshely <catrol@qq.com>
2022-04-10 00:15:45 +0800

    💾 Feat: Base.h

---
+++ Base/Base.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <iostream>
+#include <string>
+#include <list>
+#include <queue>
+#include <stack>
+
+typedef __int16 i16;
+typedef __int32 i32;
+typedef __int64 i64;
+
+typedef long double ld;
+
+typedef unsigned __int16 ui16;
+typedef unsigned __int32 ui32;
+typedef unsigned __int64 ui64;
+
+typedef unsigned char uchar;
+typedef unsigned long double uld;
+
+
